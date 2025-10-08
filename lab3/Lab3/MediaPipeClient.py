cdfaee50378bc867e8ab0e92993eba2bcd6694e9
stestefe <stefan.le@studium.uni-hamburg.de>
2025-10-08 12:57:01 +0800

    Update MediaPipeClient.py

--- lab3/Lab3/MediaPipeClient.py
+++ lab3/Lab3/MediaPipeClient.py
@@ -4,30 +4,18 @@ import cv2
 import numpy as np
 import pyrealsense2 as rs
 from MediaPipe import MediaPipe
-import socket
-import json
-import pyrealsense2 as rs
-import numpy as np
-import cv2
-from collections import defaultdict, deque
-import threading
 import time
-
 from scipy.linalg import lstsq
 
-'''The server's hostname or IP address'''
 HOST = "127.0.0.1" 
-'''The port used by the server'''
 PORT = 13456
 
 def main():
     mp = MediaPipe()
     
-    # Configure depth and color streams
     pipeline = rs.pipeline()
     config = rs.config()
 
-    # Get device product line for setting a supporting resolution
     pipeline_wrapper = rs.pipeline_wrapper(pipeline)
     pipeline_profile = config.resolve(pipeline_wrapper)
     device = pipeline_profile.get_device()
@@ -44,84 +32,101 @@ def main():
 
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-   
 
-    # Start streaming
     pipeline.start(config)
-    # Align Color and Depth
     align_to = rs.stream.color
     align = rs.align(align_to)
-    body_dict = {'head' : 0, 'leftHand': 1, 'rightHand': 2}
 
     T_matrix = None
+    calibration_samples = []
+    CALIBRATION_SAMPLES_NEEDED = 5
+    
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.connect((HOST, PORT))
             sock.setblocking(0)
+            
             while True:
-                # Wait for a coherent pair of frames: depth and color
                 frames = pipeline.wait_for_frames()
                 aligned_frames = align.process(frames)
                 depth_frame = aligned_frames.get_depth_frame()
                 color_frame = aligned_frames.get_color_frame()
-                if not  depth_frame or not color_frame:
+                
+                if not depth_frame or not color_frame:
                     continue
 
-
-                # Detect skeleton and send it to Unity
                 color_image = np.asanyarray(color_frame.get_data())
                 detection_results = mp.detect(color_image)
                 color_image = mp.draw_landmarks_on_image(color_image, detection_results)
-                # SKELETON DATA {'LHand_x': -0.17059844732284546, 'LHand_y': 1.2849622815847397, 'LHand_z': -0.6300000548362732, 'RHand_x': 0.23544804751873016, 'RHand_y': 1.3979488387703896, 'RHand_z': -0.6070000529289246, 'Head_x': 0.025743409991264343, 'Head_y': 1.6948225647211075, 'Head_z': -0.625}
+                
                 skeleton_data = mp.skeleton(color_image, detection_results, depth_frame)
                 if skeleton_data is None:
+                    cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+                    color_image = cv2.flip(color_image, 1)
+                    cv2.imshow('RealSense', color_image)
+                    cv2.waitKey(1)
                     continue
                 
-                skeleton_points = [
+                skeleton_points = np.array([
                     [skeleton_data['Head_x'], skeleton_data['Head_y'], skeleton_data['Head_z']],
                     [skeleton_data['LHand_x'], skeleton_data['LHand_y'], skeleton_data['LHand_z']],
                     [skeleton_data['RHand_x'], skeleton_data['RHand_y'], skeleton_data['RHand_z']]
-                ]
-                # if skeleton_data is not None and :
-                #      send(sock, skeleton_data)
-                # JSON => head, leftHand, rightHand
+                ])
 
-                print("SKELETON DATA", skeleton_data, flush = True)
+                print("MediaPipe Skeleton:", skeleton_data, flush=True)
+                
                 try:
                     msg = receive(sock)
                     anchors = msg['listOfAnchors']
-                    mediapipe_points = np.array([
-                        [anchor['position']['x'], anchor['position']['y'], anchor['position']['z']]
-                        for anchor in anchors
+                    
+                    unity_points = np.array([
+                        [anchors[0]['position']['x'], anchors[0]['position']['y'], anchors[0]['position']['z']],  # head
+                        [anchors[1]['position']['x'], anchors[1]['position']['y'], anchors[1]['position']['z']],  # left hand
+                        [anchors[2]['position']['x'], anchors[2]['position']['y'], anchors[2]['position']['z']]   # right hand
                     ])
-
-                    print("quest points:", mediapipe_points, flush=True)
+                    
+                    print("Unity Anchor Points:", unity_points, flush=True)
 
                     # CALIBRATION PHASE
                     if T_matrix is None:
-                        if len(mediapipe_points) == len(anchors):
-                            skeleton_homogeneous = np.hstack([skeleton_points, np.ones((len(skeleton_points), 1))])
-                            mediapipe_homogenous = np.hstack([mediapipe_points, np.ones((mediapipe_points.shape[0], 1))])
+                        calibration_samples.append((skeleton_points.copy(), unity_points.copy()))
+                        print(f"Calibration sample {len(calibration_samples)}/{CALIBRATION_SAMPLES_NEEDED} collected", flush=True)
+                        
+                        if len(calibration_samples) >= CALIBRATION_SAMPLES_NEEDED:
+                            all_skeleton = np.vstack([s for s, u in calibration_samples])
+                            all_unity = np.vstack([u for s, u in calibration_samples])
+                            
 
-                            A = skeleton_homogeneous
-                            b = mediapipe_homogenous
-
-                            T_transpose, _, _, _ = lstsq(A, b)
+                            skeleton_homogeneous = np.hstack([all_skeleton, np.ones((all_skeleton.shape[0], 1))])
+                            
+                            T_transpose, residuals, rank, s = lstsq(skeleton_homogeneous, all_unity)
                             T_matrix = T_transpose.T
-                            print("---TRANSFORMATION MATRIX CALCULATED---\n", T_matrix, flush=True)
+                            
+                            print("=" * 50)
+                            print("CALIBRATION COMPLETE!")
+                            print("T_matrix:")
+                            print(T_matrix)
 
+                    # TRACKING PHASE
                     if T_matrix is not None:
                         transformed_anchors = []
-                        for index, mediapipe_point in enumerate(mediapipe_points):
-                            homogenous_mediapipe_point = np.array([mediapipe_point[0], mediapipe_point[1], mediapipe_point[2], 1.0])
-                            transformed_point = (T_matrix @ homogenous_mediapipe_point)[:3]
-
+                        
+                        for index, skeleton_point in enumerate(skeleton_points):
+                            homogenous_skeleton_point = np.array([
+                                skeleton_point[0], 
+                                skeleton_point[1], 
+                                skeleton_point[2], 
+                                1.0
+                            ])
+                            
+                            transformed_point = (T_matrix @ homogenous_skeleton_point)[:3]
+                            
                             transformed_anchor = {
                                 'anchor_id': index,
                                 'original_position': {
-                                    'x': float(mediapipe_point[0]),
-                                    'y': float(mediapipe_point[1]),
-                                    'z': float(mediapipe_point[2]),
+                                    'x': float(skeleton_point[0]),
+                                    'y': float(skeleton_point[1]),
+                                    'z': float(skeleton_point[2]),
                                 },
                                 'transformed_position': {
                                     'x': float(transformed_point[0]),
@@ -131,41 +136,41 @@ def main():
                             }
                             transformed_anchors.append(transformed_anchor)
                         
-                        response_msg = {
-                            'transformedAnchors': transformed_anchors
-                        }
-                        print(f"Sending {len(transformed_anchors)} visible mediapipe landmarks:", response_msg, flush=True)
+                        response_msg = {'transformedAnchors': transformed_anchors}
+                        print(f"------ Sending transformed positions:", response_msg, flush=True)
                         send(sock, response_msg)
                     else:
-                        response_msg = {
-                            'transformedAnchors': []
-                        }
-                        print("No markers visible - sending empty message", flush=True)
+                        response_msg = {'transformedAnchors': []}
                         send(sock, response_msg)
-                    time.sleep(0.2)
-                except:
-                    print("----------------ERROR has occured----------------")
+                    
+                    time.sleep(0.05)
+                    
+                except BlockingIOError:
                     pass
-                # Show images
+                except Exception as e:
+                    print(f"ERROR: {e}", flush=True)
+                
+                # Display
                 cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
                 color_image = cv2.flip(color_image, 1)
                 cv2.imshow('RealSense', color_image)
-                cv2.waitKey(1)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+                    
     finally:
-        # Stop streaming
         pipeline.stop()
+        cv2.destroyAllWindows()
 
 def receive(sock):
-    data = sock.recv(1024)
+    data = sock.recv(4096)
     data = data.decode('utf-8')
     msg = json.loads(data)
-    print("Received:", msg, flush=True)
+    print("Received from Unity:", msg, flush=True)
     return msg
 
 def send(sock, msg):
     data = json.dumps(msg)
     sock.sendall(data.encode('utf-8'))
-    print("Sent: ", msg)
 
 if __name__ == '__main__':
     main()
