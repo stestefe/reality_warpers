e9d1cc513c22c6911e2357832dd01e9b55d50b55
Natesd05 <natesd05@gmail.com>
2025-10-14 12:53:43 +0800

    added randome spawning and destroying

---
+++ lab4/Lab4_Unity/Assets/DestroyGameObject.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnTrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Cart"))
+        {
+            Destroy(other.gameObject);
+
+            GameObject spawner = GameObject.FindObjectOfType<SpawnGameObject>()?.gameObject;
+            if (spawner != null)
+            {
+                spawner.GetComponent<SpawnGameObject>().RemoveFromList(other.gameObject);
+            }
+        }
+    }
+}
---
+++ lab4/Lab4_Unity/Assets/SpawnGameObject.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGameObject : MonoBehaviour
+{
+    public GameObject gameObject1Prefab;
+    public int maxObjects = 10;
+    public Vector2 planeSize = new Vector2(10f, 10f);
+    public float padding = 1f;
+    public List<GameObject> spawnedObjects = new List<GameObject>();
+
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnObject), 1f, 2f);
+    }
+
+    void SpawnObject()
+    {
+        if (spawnedObjects.Count >= maxObjects)
+            return;
+
+        float halfX = planeSize.x / 2f - padding;
+        float halfZ = planeSize.y / 2f - padding;
+
+        if (halfX <= 0 || halfZ <= 0)
+        {
+            return;
+        }
+
+        float x = Random.Range(-halfX, halfX);
+        float z = Random.Range(-halfZ, halfZ);
+        Vector3 spawnPos = new Vector3(x, 0.5f, z);
+
+        GameObject obj = Instantiate(gameObject1Prefab, spawnPos, Quaternion.identity);
+        spawnedObjects.Add(obj);
+    }
+
+    public void RemoveFromList(GameObject obj)
+    {
+        spawnedObjects.Remove(obj);
+    }
+}
+
