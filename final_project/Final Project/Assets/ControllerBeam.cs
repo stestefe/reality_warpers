e9d2563e2b4f9790c0982eef07ec488f02fdcab3
Natesd05 <natesd05@gmail.com>
2025-12-15 16:41:50 +0800

    added blackscreen feature

--- final_project/Final Project/Assets/ControllerBeam.cs
+++ final_project/Final Project/Assets/ControllerBeam.cs
@@ -8,15 +8,20 @@ public class ControllerBeam : NetworkBehaviour
     public Vector3 beamScale = new Vector3(0.05f, 0.05f, 1f);
     public Material beamMaterial;
     public InputActionProperty triggerAction;
+    public InputActionProperty buttonAction; // New: Button input
     public string targetTag = "Player";
     public GameObject passThroughPlane;
-
-    // public GameObject clientObjectToEnable;
 
     private GameObject beamCube;
     private BoxCollider beamCollider;
 
     public NetworkVariable<bool> isColliding = new NetworkVariable<bool>(
+        false, 
+        NetworkVariableReadPermission.Everyone, 
+        NetworkVariableWritePermission.Owner
+    );
+    
+    public NetworkVariable<bool> buttonTriggered = new NetworkVariable<bool>(
         false, 
         NetworkVariableReadPermission.Everyone, 
         NetworkVariableWritePermission.Owner
@@ -40,6 +45,7 @@ public class ControllerBeam : NetworkBehaviour
         beamCube.SetActive(false);
 
         triggerAction.action.Enable();
+        buttonAction.action.Enable();
 
         BeamCollisionDetector detector = beamCube.AddComponent<BeamCollisionDetector>();
         detector.Initialize(this, targetTag);
@@ -50,6 +56,7 @@ public class ControllerBeam : NetworkBehaviour
         base.OnNetworkSpawn();
         
         isColliding.OnValueChanged += OnCollidingChanged;
+        buttonTriggered.OnValueChanged += OnButtonTriggeredChanged; // New
         
         OnCollidingChanged(false, isColliding.Value);
     }
@@ -58,6 +65,7 @@ public class ControllerBeam : NetworkBehaviour
     {
         base.OnNetworkDespawn();
         isColliding.OnValueChanged -= OnCollidingChanged;
+        buttonTriggered.OnValueChanged -= OnButtonTriggeredChanged; // New
     }
 
     void Update()
@@ -72,6 +80,12 @@ public class ControllerBeam : NetworkBehaviour
             beamCube.transform.localRotation = Quaternion.identity;
             beamCube.transform.localScale = beamScale;
             beamCube.transform.localPosition = new Vector3(0, 0, beamScale.z / 2f);
+        }
+
+        if (buttonAction.action.WasPressedThisFrame() && isColliding.Value)
+        {
+            buttonTriggered.Value = true;
+            Debug.Log("Button pressed while colliding!");
         }
     }
 
@@ -98,12 +112,11 @@ public class ControllerBeam : NetworkBehaviour
     private void OnCollidingChanged(bool previousValue, bool newValue)
     {
         Debug.Log($"Collision state changed: {newValue}");
-        
-        // if (clientObjectToEnable != null)
-        // {
-        //     clientObjectToEnable.SetActive(newValue);
-        // }
+    }
 
+    private void OnButtonTriggeredChanged(bool previousValue, bool newValue)
+    {
+        Debug.Log($"Button trigger state changed: {newValue}");
     }
 
     public bool IsColliding => isColliding.Value;
--- final_project/Final Project/Assets/ControllerBeamResponder.cs
+++ final_project/Final Project/Assets/ControllerBeamResponder.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
 using Unity.Netcode;
+using System.Collections;
 
 public class ClientBeamResponder : MonoBehaviour
 {
     public GameObject objectToEnable;
+    public GameObject objectToEnable2;
+    public float activeDuration = 10f;
+    
     private ControllerBeam hostBeam;
+    private Coroutine timedActivationCoroutine;
 
     void Start()
     {
         StartCoroutine(FindHostBeam());
     }
 
-    private System.Collections.IEnumerator FindHostBeam()
+    private IEnumerator FindHostBeam()
     {
         yield return new WaitForSeconds(0.5f);
         
@@ -20,6 +25,7 @@ public class ClientBeamResponder : MonoBehaviour
         if (hostBeam != null)
         {
             hostBeam.isColliding.OnValueChanged += OnHostBeamCollisionChanged;
+            hostBeam.buttonTriggered.OnValueChanged += OnButtonTriggeredChanged;
             
             OnHostBeamCollisionChanged(false, hostBeam.IsColliding);
             
@@ -41,11 +47,51 @@ public class ClientBeamResponder : MonoBehaviour
         }
     }
 
+    private void OnButtonTriggeredChanged(bool oldValue, bool newValue)
+    {
+        Debug.Log($"Client detected button trigger: {newValue}");
+        
+        if (newValue && objectToEnable2 != null)
+        {
+            if (timedActivationCoroutine != null)
+            {
+                StopCoroutine(timedActivationCoroutine);
+            }
+
+            timedActivationCoroutine = StartCoroutine(ActivateForDuration());
+            
+            if (hostBeam != null && hostBeam.IsOwner)
+            {
+                hostBeam.buttonTriggered.Value = false;
+            }
+        }
+    }
+
+    private IEnumerator ActivateForDuration()
+    {
+        objectToEnable2.SetActive(true);
+        Debug.Log($"Activated {objectToEnable2.name} for {activeDuration} seconds");
+        
+        yield return new WaitForSeconds(activeDuration);
+        
+        objectToEnable2.SetActive(false);
+        Debug.Log($"Deactivated {objectToEnable2.name}");
+        
+        timedActivationCoroutine = null;
+    }
+
     void OnDestroy()
     {
         if (hostBeam != null)
         {
             hostBeam.isColliding.OnValueChanged -= OnHostBeamCollisionChanged;
+            hostBeam.buttonTriggered.OnValueChanged -= OnButtonTriggeredChanged; // New
+        }
+        
+        // Clean up any running coroutines
+        if (timedActivationCoroutine != null)
+        {
+            StopCoroutine(timedActivationCoroutine);
         }
     }
 }
